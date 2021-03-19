d635b8ffd881f9d5b463e2d29c2b020cfde98324
Fahia Tasmin <tasminscourse@gmail.com>
2021-03-19 20:20:16 +0600

    final

--- src/component/ProductDetails/ProductDetails.js
+++ src/component/ProductDetails/ProductDetails.js
@@ -6,7 +6,7 @@ import Product from '../Product/Product';
 const ProductDetails = () => {
     
     const {productKey} = useParams();
-    const product = fakeData.find(pd => pd.key == productKey);
+    const product = fakeData.find(pd => pd.key === productKey);
     console.log(product);
     return (
         <div>
